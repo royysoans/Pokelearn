ac569b001a990bf5b4413db8de0c7df2c1abe481
royysoans <roystonsoans3@gmail.com>
2025-11-02 00:05:45 +0530

    Pokeball as a cursor

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Cursor from "./components/Cursor";
 
 const queryClient = new QueryClient();
 
@@ -34,6 +35,7 @@ const App = () => {
         <TooltipProvider>
           <Toaster />
           <Sonner />
+          <Cursor />
           {/* Global mute button */}
           <button
             onClick={toggleMute}
