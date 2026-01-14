4d66d3079fe04a23c682de7ad881731517c82a58
Royston Soans <roystonsoans3@gmail.com>
2026-01-14 12:00:16 +0530

    Optimizing

--- disable_rls.sql
+++
@@ -1,9 +0,0 @@
-DO $$
-BEGIN
-    -- Disable RLS on user_pokemons to allow full access
-    -- This confirms if RLS policies were the issue
-    ALTER TABLE user_pokemons DISABLE ROW LEVEL SECURITY;
-    
-    -- Also ensure profiles are visible
-    ALTER TABLE profiles DISABLE ROW LEVEL SECURITY;
-END $$;
